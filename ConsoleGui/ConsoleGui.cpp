7f6db78e61a0f132357bbd9366a501ccbbafbaba
KleskBY <klesksp@gmail.com>
2019-12-17 18:59:25 -0500

    1.0

---
+++ ConsoleGui/ConsoleGui.cpp
@@ -0,0 +1,56 @@
+﻿#include "ConGui.h"
+//std::remove("log.log");
+//freopen("log.log", "w", stdout);
+
+bool mybool = false;
+int myint = 40;
+float myfloat = 100.f;
+std::string mystring = "INPUTTEXT";
+int main()
+{
+	ConGui::ApplyConsoleStyle();
+	ConGui::PrepareFrame();
+	ConGui::Init();
+	ConGui::SetCursorState(false);
+	
+	while (true)
+	{
+		Sleep(30);
+		if (!IsWindowVisible(ConGui::WindowHWND)) continue;
+
+		ConGui::Frame();
+		ConGui::InputHandle();
+
+		//ConGui::Text(ConGui::CursorX, ConGui::CursorY, "x");
+		//ConGui::screen[1 * ConGui::ConsoleWidth + 1] = 0x25A0;
+
+		ConGui::Text(0, 0, ("X=" + std::to_string(ConGui::CursorX) + " Y=" + std::to_string(ConGui::CursorY) + " S=" + std::to_string(ConGui::SelectedBlock) + " X=" + std::to_string(ConGui::LastX)).c_str(), 10);
+		ConGui::Style::BoxStyle = ConGui::Style::BoxStyle_Default;
+		if (ConGui::Button("BUTTON 1", 2, 2, 20, 4)) Beep(100, 100);
+		ConGui::Style::BoxStyle = ConGui::Style::BoxStyle_Single;
+		if (ConGui::Button("BUTTON 2", 2, 5, 20, 7)) Beep(200, 200);
+		ConGui::Style::BoxStyle = ConGui::Style::BoxStyle_Corners;
+		if (ConGui::Button("BUTTON 3", 2, 8, 20, 10)) Beep(300, 300);
+		ConGui::Style::BoxStyle = ConGui::Style::BoxStyle_Corners2;
+		if (ConGui::Button("BUTTON 4", 22, 2, 44, 4)) Beep(400, 100);
+		ConGui::Style::BoxStyle = ConGui::Style::BoxStyle_Combo;
+		if (ConGui::Button("BUTTON 5", 22, 5, 44, 7)) Beep(500, 100);
+		ConGui::Style::BoxStyle = ConGui::Style::BoxStyle_Combo2;
+		if (ConGui::Button("BUTTON 6", 22, 8, 44, 10)) Beep(500, 100);
+
+		ConGui::Style::CheckBoxStyle = ConGui::Style::CheckBoxStyle_Round;
+		ConGui::CheckBox("Checkbox1", 2, 12, &mybool); 
+		ConGui::Style::CheckBoxStyle = 0;
+		ConGui::CheckBox("Checkbox2", 16, 12, &mybool);
+		ConGui::Style::CheckBoxStyle = ConGui::Style::CheckBoxStyle_BigBox;
+		ConGui::CheckBox("Checkbox3", 30, 11, &mybool);
+		ConGui::Style::CheckBoxStyle = ConGui::Style::CheckBoxStyle_Rect;
+		ConGui::CheckBox("Checkbox4", 46, 12, &mybool);
+
+		ConGui::SliderInt(2, 14,20, &myint);
+		ConGui::SliderFloat(2, 16, 20, &myfloat,0,200,5.5f);
+		ConGui::InputText("INPUT TEXT:", 46, 2, 17, &mystring);
+
+		ConGui::Render();
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ConGui
  Console gui for windows
+<br>
+
+<b>Features:</b><br>
+- Simple arrows menu (INSERT, arrows)<br>
+- Aimbot (ALT key,buggy as fuck)<br>
+- ESP (buggy as fuck)<br>
+- Unhook (buggy as fuck)<br>
+
+<b>Credits:</b><br>
+- KleskBY (Author, reverse, coding)<br>
+- ocornut (imgui)<br>
+- TsudaKageyu (minhook)<br>
+- TheFlood (memory class)<br>
+- RangeMachine (imgui render)<br>
+
+![alt text](https://github.com/KleskBY/UrbanTerrorCheat/blob/master/img.jpg?raw=true)<br>
